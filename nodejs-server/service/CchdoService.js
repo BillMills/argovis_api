e280336dccbbdb5ae4d816fd13c7b6e51d8fba53
bkatiemills <you@example.com>
2024-05-15 20:00:27 -0400

    distracting reference to multipolygon

--- nodejs-server/service/CchdoService.js
+++ nodejs-server/service/CchdoService.js
@@ -28,7 +28,7 @@ exports.cchdoVocab = function(parameter) {
  * endDate Date ISO 8601 UTC date-time formatted string indicating the end of the time period of interest. (optional)
  * polygon String array of [lon, lat] vertices describing a polygon bounding the region of interest; final point must match initial point (optional)
  * box String lon, lat pairs of the lower left and upper right corners of a box on a mercator projection, packed like [[lower left lon, lower left lat],[upper right lon, upper right lat]] (optional)
- * winding String Enforce ccw winding for polygon and multipolygon (optional)
+ * winding String Enforce ccw winding for polygon (optional)
  * center List center to measure max radius from when defining circular region of interest; must be used in conjunction with query string parameter 'radius'. (optional)
  * radius BigDecimal km from centerpoint when defining circular region of interest; must be used in conjunction with query string parameter 'center'. (optional)
  * metadata String metadata pointer (optional)
